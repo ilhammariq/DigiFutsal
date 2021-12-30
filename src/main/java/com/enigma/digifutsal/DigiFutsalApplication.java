a40e1093a527caf430c1462bc35e5c4f56419ff7
ilhammariq <ilhammuhammadariq123@gmail.com>
2021-12-30 21:37:27 +0700

    initial commit

---
+++ src/main/java/com/enigma/digifutsal/DigiFutsalApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.digifutsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiFutsalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiFutsalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/digifutsal/DigiFutsalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.digifutsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiFutsalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
